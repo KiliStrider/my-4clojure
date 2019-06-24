92bdf3e5b201936deb8a8a1b0795fcc860ba13b9
Gill Nitzan <gill@cycognito.com>
2019-06-24 17:42:16 +0300

    Question 73 - Analyze a Tic-Tac-Toe Board

--- src/my_4clojure/hard.clj
+++ src/my_4clojure/hard.clj
@@ -27,4 +27,53 @@
   (is (= (q-53 [7 6 5 4]) [])))
 
 
+; #73 - Analyze a Tic-Tac-Toe Board
+(def q-73 (fn [r]
+            (let [cg (map (fn [n] (fn [s] (apply vector (map #(get % n) s)))) (range 3))
+                  c (map #(% r) cg)
+                  dg (map (fn [n] (fn [s] (get s n))) (range 3))
+                  d [(apply vector (map #(%1 %2) dg r)) (apply vector (map #(%1 %2) dg (reverse r)))] ]
+              (first (filter
+                       #(not (= % nil))
+                       (map
+                         (fn [s]
+                           (reduce #(when (and (= %1 %2) (not (= %1 :e))) %2) s))
+                         (concat r c d))))
+              )))
+
+(deftest q-73-test-1
+  (is (= nil (q-73 [[:e :e :e]
+                    [:e :e :e]
+                    [:e :e :e]]))))
+
+(deftest q-73-test-2
+  (is (= :x (q-73 [[:x :e :o]
+                   [:x :e :e]
+                   [:x :e :o]]))))
+
+(deftest q-73-test-3
+  (is (= :o (q-73 [[:e :x :e]
+                   [:o :o :o]
+                   [:x :e :x]]))))
+
+(deftest q-73-test-4
+  (is (= nil (q-73 [[:x :e :o]
+                    [:x :x :e]
+                    [:o :x :o]]))))
+
+(deftest q-73-test-5
+  (is (= :x (q-73 [[:x :e :e]
+                   [:o :x :e]
+                   [:o :e :x]]))))
+
+(deftest q-73-test-6
+  (is (= :o (q-73 [[:x :e :o]
+                   [:x :o :e]
+                   [:o :e :x]]))))
+
+(deftest q-73-test-7
+  (is (= nil (q-73 [[:x :o :x]
+                    [:x :o :x]
+                    [:o :x :o]]))))
+
 (run-tests)
