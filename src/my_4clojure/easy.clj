0ff1ee4fccc79e9fa749e7b608075c4b3cbc935f
Gill Nitzan <gill@cycognito.com>
2019-06-25 11:17:13 +0300

    Question 32 - Duplicate a Sequence, Question 33 - Replicate a Sequence

--- src/my_4clojure/easy.clj
+++ src/my_4clojure/easy.clj
@@ -7,3 +7,12 @@
             (take n ((fn nf
                        [a b]
                        (lazy-seq (cons b (nf b (+ a b))))) 0 1 ))))
+
+; #32 - Duplicate a Sequence
+(def q-32 (fn [s]
+            (reduce into (apply map vector (repeat 2 s)))))
+
+
+; #33 - Replicate a Sequence
+(def q-33 (fn [s n]
+            (reduce into (apply map vector (repeat n s)))))
