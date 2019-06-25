39971c586eaf3dcaf967c2ddada51274579fdea8
Gill Nitzan <gill@cycognito.com>
2019-06-25 19:47:01 +0300

    Question 34 - Implement range, Question 41 - Drop Every Nth Item, Question 42 - Factorial Fun

--- src/my_4clojure/easy.clj
+++ src/my_4clojure/easy.clj
@@ -16,3 +16,21 @@
 ; #33 - Replicate a Sequence
 (def q-33 (fn [s n]
             (reduce into (apply map vector (repeat n s)))))
+
+; #34 - Implement range
+(def q-34 (fn
+            [n m]
+            (take (- m n)
+                  ((fn nf [a]
+                       (lazy-seq (cons a (nf (inc a))))) n ))))
+
+; #41 - Drop Every Nth Item
+(def q-41 (fn
+            [s n]
+            (reduce #(concat %1 (take (- n 1) %2))
+                    []
+                    (partition-all n s))
+            ))
+
+; #42 - Factorial Fun
+(def q-42 #(reduce * (range 1 (+ % 1))))
