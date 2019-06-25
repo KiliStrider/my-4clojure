2778493e92773f828e6743abb3b7f1f35453ff93
Gill Nitzan <gill@cycognito.com>
2019-06-25 20:43:35 +0300

    Question 43 - Reverse Interleave, Question 44 - Rotate Sequence

--- src/my_4clojure/medium.clj
+++ src/my_4clojure/medium.clj
@@ -2,6 +2,14 @@
 (use 'clojure.test)
 
 
+; #43 - Reverse Interleave
+(def q-43 #(map (fn [s] (take-nth %2 (drop s %1))) (range %2)))
+
+; #44 - Rotate Sequence
+(def q-44 (fn [n s]
+            (flatten (map #(% (mod n (count s)) s)
+                          [drop take]))))
+
 ; #74 - Filter Perfect Squares
 (def q-74 (fn [s]
             (clojure.string/join
@@ -11,3 +19,5 @@
                   (> (count (filter #(= n (* % %)) (range n))) 0))
                 (map read-string (clojure.string/split s #","))))
             ))
+
+
