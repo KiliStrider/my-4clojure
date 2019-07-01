c34e92959a46b7bcbf5b957837be6e0058c3830d
Gill Nitzan <gill@cycognito.com>
2019-07-01 15:44:14 +0300

    refactored solve-knights-tour

--- src/my_4clojure/misc.clj
+++ src/my_4clojure/misc.clj
@@ -1,9 +1,15 @@
 (ns my-4clojure.misc)
 
+(defn get-current-position-number [board]
+  (->> board
+       (apply concat)
+       (apply max)))
+
 (defn get-current-position [board]
-  (let [max-turn (apply max (flatten board))]
-    (first (for [r (range 8)
-                 c (range 8)
+  (let [max-turn (get-current-position-number board)
+        board-length (count board)]
+    (first (for [r (range board-length)
+                 c (range board-length)
                  :when (= max-turn (get-in board [r c]))]
              [r c]))))
 
@@ -25,7 +31,7 @@
     (filter #(valid-move? board %) all-moves)))
 
 (defn move-knight [board [row-dest col-dest]]
-  (let [next-move (+ 1 (apply max (flatten board)))
+  (let [next-move (+ 1 (get-current-position-number board))
         board-length (count board)]
     (vec (for [r (range board-length)]
            (if (= row-dest r)
@@ -39,31 +45,38 @@
 (defn move-accessibility [board move]
   (count (get-next-moves (move-knight board move))))
 
+(defn completed-tour? [board ]
+  (let [board-length (count board)
+        next-move (inc (get-current-position-number board))]
+    (> next-move (* board-length board-length))))
 
 (defn solve-knights-tour-rec [board]
-  (let [board-length (count board)
-        next-move (+ 1 (apply max (flatten board)))
-        completed-tour (> next-move (* board-length board-length))
-        next-moves (sort-by (partial move-accessibility board) (get-next-moves board))]
-    (if completed-tour
+  (let [next-moves (sort-by (partial move-accessibility board) (get-next-moves board))]
+    (if (completed-tour? board)
       board
       (first (filter some? (map #(solve-knights-tour-rec (move-knight board %)) next-moves))))))
 
-(defn solve-knights-tour [n]
-       (let [board (vec (cons (into [1] (vec (repeat (- n 1) 0)))
-                              (vec (repeat (- n 1) (vec (repeat n 0))))))]
-         (solve-knights-tour-rec board)))
+(defn create-random-board [n]
+  (vec (map vec (partition n
+                           (-> (repeat (* n n) 0)
+                               vec
+                               (assoc (rand-int (* n n)) 1))))))
 
-(defmacro time-sec
-  "Evaluates expr and prints the time it took.  Returns the value of
- expr."
-  {:added "1.0"}
-  [expr]
-  `(let [start# (. System (nanoTime))
-         ret# ~expr]
-     (prn (str "Elapsed time: " (/ (double (- (. System (nanoTime)) start#)) 1000000.0) " msecs"))
-     ret#))
+(defn solve-knights-tour [n]
+       (let [board (create-random-board n)]
+         (solve-knights-tour-rec board)))
 
 
 (comment
-  ((time-sec solve-knights-tour) 5))
+  (solve-knights-tour 8)
+  (create-random-board 9)
+  (solve-knights-tour-rec [[1 0 0 0 0 0 0 0 0]
+                           [0 0 0 0 0 0 0 0 0]
+                           [0 0 0 0 0 0 0 0 0]
+                           [0 0 0 0 0 0 0 0 0]
+                           [0 0 0 0 0 0 0 0 0]
+                           [0 0 0 0 0 0 0 0 0]
+                           [0 0 0 0 0 0 0 0 0]
+                           [0 0 0 0 0 0 0 0 0]
+                           [0 0 0 0 0 0 0 0 0]]))
+
